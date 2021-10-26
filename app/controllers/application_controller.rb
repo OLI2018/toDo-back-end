d00d57c981527972bb78b0ff7cda0243144be973
Oleg <roxvemm2014@gmail.com>
2021-10-25 21:08:03 -0700

    my project

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,9 +1,35 @@
+
 class ApplicationController < Sinatra::Base
+
   set :default_content_type, 'application/json'
-  
-  # Add your routes here
-  get "/" do
-    { message: "Good luck with your project!" }.to_json
+
+  # get '/categories' do
+  #     categories = Category.all
+  #     categories.to_json
+  # end
+
+  get '/todos' do
+    todos = Todo.all
+    todos.to_json
+end
+
+
+
+delete '/todos/:id' do
+  # find the review using the ID
+  todo = Todo.find(params[:id])
+  # delete the review
+  todo.destroy
+  # send a response with the deleted review as JSON
+  todo.to_json
+end
+
+
+
+  get '/categories/:id' do
+    category = Category.find(params[:id])
+    category.to_json(include: :todos)
   end
 
 end
+
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,5 +1,30 @@
 puts "🌱 Seeding spices..."
 
-# Seed your database here
+# todo_text = ["call mom", "go to gym", "pick up children after school", "build a cool API", "go to work"]
+
+# Category.create(name: "Code")
+# Category.create(name: "Food")
+# Category.create(name: "Money")
+# Category.create(name: "Misc")
+
+# Category.all.each do |category|
+#     2.times do
+#         Todo.create(text: todo_text.sample, category_id: category.id)
+#   end
+# end
+
+
+Category.create(name: "Code")
+Category.create(name: "Food")
+Category.create(name: "Money")
+Category.create(name: "Misc")
+
+Todo.create(text: "call mom", category_id: 4)
+Todo.create(text: "go to gym", category_id: 4)
+Todo.create(text: "pick up children after school", category_id: 4)
+Todo.create(text: "build a cool API", category_id: 1)
+Todo.create(text: "make a meal", category_id: 2)
+Todo.create(text: "go to the bank", category_id: 3)
+Todo.create(text: "go to work", category_id: 3)
 
 puts "✅ Done seeding!"
